89cb84ca1472b0ae308653ceffb4643732bc945e
Code bee <codebee345@outlook.com>
2023-10-01 14:24:07 +0100

    added uploading in chunks

--- chromeExt/admin.py
+++ chromeExt/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from chromeExt.models import Video
+from chromeExt.models import Video, VideoSession
 
 # Register your models here.
 admin.site.register(Video)
+admin.site.register(VideoSession)
